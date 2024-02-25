720c9193592b6df0d90c5911b570e00085918d14
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-02-25 20:37:05 +0100

    feat: deleting cart items

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -10,7 +10,7 @@ function Cart() {
   const cart = useSelector(getCart);
   const dispatch = useDispatch();
 
-  if(!cart.length) return <EmptyCart />;
+  if (!cart.length) return <EmptyCart />;
 
   return (
     <div className='px-4 py-3'>
@@ -19,7 +19,7 @@ function Cart() {
       <h2 className='mt-7 text-xl font-semibold'>Your cart, {username}</h2>
 
       <ul className='mt-3 divide-y divide-stone-200 border-b'>
-        {cart.map((item) => <CartItem item={item} key={item.key} />)}
+        {cart.map((item) => <CartItem item={item} key={item.id} />)}
       </ul>
 
       <div className='mt-6 space-x-2'>
